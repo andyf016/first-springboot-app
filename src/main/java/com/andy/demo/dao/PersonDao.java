848bd0b9f926336fcca66b2bc6769722775c625d
Andrew Fillenwarth <andyf016@gmail.com>
2020-11-25 21:37:54 -0500

    first springboot ap

---
+++ src/main/java/com/andy/demo/dao/PersonDao.java
@@ -0,0 +1,15 @@
+package com.andy.demo.dao;
+
+import com.andy.demo.model.Person;
+
+import java.util.UUID;
+
+public interface PersonDao {
+
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson(Person person){
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+}
---
+++ src/main/java/com/andy/demo/service/PersonService.java
@@ -0,0 +1,22 @@
+package com.andy.demo.service;
+
+import com.andy.demo.dao.PersonDao;
+import com.andy.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person){
+        return personDao.insertPerson(person);
+    }
+}
